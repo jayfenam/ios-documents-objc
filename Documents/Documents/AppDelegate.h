0ae8d7f19ff075a94b42da0e56476500c2987039
jayfenam <jonathanferrer95@gmail.com>
2019-07-17 22:23:52 -0400

    added project

---
+++ Documents/Documents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Documents
+//
+//  Created by Jonathan Ferrer on 7/17/19.
+//  Copyright © 2019 Jonathan Ferrer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Documents/Documents/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Documents
+//
+//  Created by Jonathan Ferrer on 7/17/19.
+//  Copyright © 2019 Jonathan Ferrer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
